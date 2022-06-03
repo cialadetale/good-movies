b336c53e0dbdd53a2c64d2a2a4ad17eaad31b4e5
Patka <patrycja.jankowska00@gmail.com>
2022-06-04 00:19:37 +0200

    Django project - movie website

---
+++ goodmovies/goodmovies/urls.py
@@ -0,0 +1,32 @@
+"""goodmovies URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from movies import views
+from movies.views import MovieListView
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('movies/', views.movie_list, name="movie_list"),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('accounts/profile/', views.profile_view, name='user_profile'),
+    path('accounts/signup/', views.user_signup, name="user_signup"),
+    path('accounts/login/', include('django.contrib.auth.urls'), name='login'),
+    path('admin/', admin.site.urls),
+    path('about/', MovieListView.as_view()),
+]    
+
+
---
+++ goodmovies/movies/views.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import re
+from django.shortcuts import render
+from movies.models import Movie
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movie_list.html"
+
+def movie_list(request):
+    movies_from_db = Movie.objects.all()
+    context = {
+        "movies" : movies_from_db,
+        "date" : datetime.now()
+    }
+    return render(request, "movie_list.html", context)
+
+def profile_view(request):
+    return render(request, "profile.html")
+
+def user_signup(request):
+    if request.method == 'POST':
+        # tu trzeba przetworzyć dane z formularza
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, template_name="registration/signup_complete.html")
+    else:
+        # tutaj obsługujemy przypadek kiedy użytkownik pierwszy raz wyświetlił stronę
+        form = UserCreationForm()
+
+    # na końcu zwracamy wyrenderowanego HTMLa
+    return render(request, template_name="registration/signup_form.html", context={'form':
+    form})
+
+def login(request):
+    return render(request, "login.html")
+
