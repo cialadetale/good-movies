6536099c618319232c0fe17a325fafed7e80e97f
Patka <patrycja.jankowska00@gmail.com>
2022-06-04 13:25:06 +0200

    Added tests to app

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+from turtle import title
 from django.test import TestCase
+from .models import Movie
 
 # Create your tests here.
+
+class TestMovies(TestCase):
+    def setUp(self):
+        Movie.objects.create(title="test", published_at=datetime.now())
+        Movie.objects.create(title="shrek", published_at=datetime.now())
+
+    def test_true(self):
+        assert 1 == 1
+
+    def test_get_movie(self):
+        assert Movie.objects.get(title="test")
+        assert Movie.objects.get(title="shrek")
+
+    def test_movie_count(self):
+        assert Movie.objects.count() == 2
